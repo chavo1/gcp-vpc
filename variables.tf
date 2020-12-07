6300b5288c4ac5fc68cd01f961fde61d1e0f23a3
chavdar <chavdar.rakov@gmail.com>
2020-12-07 16:57:43 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 variable "region" {}
 variable "region_zone" {}
 variable "project_name" {}
-variable "credentials_file_path" {}
 variable "source_service_accounts" {}
 variable "remote_cidr" {}
 variable "fw_source_range" {}
